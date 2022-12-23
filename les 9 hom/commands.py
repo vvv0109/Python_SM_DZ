675b8f4a40efb165fcee7b3875da0095452d2b97
Vadim <qwe@ya.ru>
2022-12-24 00:40:17 +0300

    10 dz

--- les 9 hom/commands.py
+++ les 9 hom/commands.py
@@ -1,9 +1,10 @@
 from unittest.mock import call
-import random
+from random import randint
 from aiogram.types import InputFile
 from config import dp, bot
 from aiogram import types
 from aiogram.dispatcher.filters import Text
+flag=str()
 total=150
 take=150
 @dp.message_handler(commands=['start'])
@@ -19,33 +20,42 @@ async def start_bot(message: types.Message):
     keyboard.add(button_2)
     photo = InputFile("12.png")
     await bot.send_photo(chat_id=message.chat.id, photo=photo, reply_markup=keyboard)
-@dp.message_handler(commands=['startr'])
-async def choice_bot(message: types.Message):
-    flag = (message.text)
-    if flag=="Да ну...пойду отсюда":
-        await message.reply(f'!!! СЛАБАК!!!')
-    else:
-        await message.reply(f'{message.from_user.first_name}, НАЧНЁМ ИГРУ ')
 @dp.message_handler()
 async def all_bot(message: types.Message):
+    global flag
     global total
     flag = (message.text)
-    if flag=="Да ну...пойду отсюда":
-        await message.reply(f'!!! СЛАБАК!!! ОТКАЗАТЬСЯ НЕЛЬЗЯ!!!')
-    await message.reply(f'{message.from_user.first_name}, НАЧНЁМ ИГРУ ')
-    take = (message.text)
+    if (flag == "Да ну...пойду отсюда"):
+        await message.reply(f'!!! СЛАБАК!!!')
+        photo = InputFile("13.jpg")
+        await bot.send_photo(chat_id=message.chat.id, photo=photo)
+        total=150
+    elif(flag == "Всё или ничего! Погнали" ):
+        await message.reply(f'{message.from_user.first_name}, НАЧНЁМ ИГРУ, ВВЕДИ ЧИСЛО ')
+    print(flag)
+    take = flag
     print(take)
     take = int(take)
-    print(f'{message.from_user.first_name} взял {take}' )
     if 0 < take <29:
          total-=int(message.text)
-         await message.reply(f'{message.from_user.first_name}, взял {message.text} конфет, осталось {total} ')
+         await message.reply(f'{message.from_user.first_name}, взял(а) {message.text} конфет, осталось {total} ')
     else:
-         await message.reply(f'!!! Ошибка, введи допустимо значение конфет!!!')
-    q=random.randint(0, 29)
-    total -= q
-    await message.reply(f'Бот, взял {q} конфет, осталось {total} ')
-    return
+         await message.reply(f'!!! Ты можешь взять только 29 конфет. Ход переходит Пиле!!!')
+    if total<=29:
+        await message.reply(f'Игра окончена!, Ты СВОБОДЕН')
+        total=150
+    move = total % 29
+    if move == 0:
+        move = randint(1, 29) if total >= 29 else total
+    total -= move
+    await message.reply(f'Пила, взял {move} конфет, осталось {total} ')
+    if total<=29:
+        await message.reply(f'Ты можешь взять не больше 28 конфет, после твоего хода оснется не больше 28 конфет на столе и ПИЛА их все забирает, Игра окончена!,выиграл Пила ')
+        photo = InputFile("13.jpg")
+        await bot.send_photo(chat_id=message.chat.id, photo=photo)
+        total=150
+
+
 
 
 
